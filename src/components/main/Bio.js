b67971f82a052eb02bebe3486970bf17f3c23265
0andme <yu00mi97@gmail.com>
2022-03-18 17:31:38 +0900

    #3 feat : 바이오 기능 추가
    - 이미지 영역 추가
    - 사용자 이름 추가
    - 블로그 설명 추가
    - 깃헙 링크 추가
    - about 이동 태그 추가

--- src/components/main/Bio.js
+++ src/components/main/Bio.js
@@ -6,10 +6,11 @@
  */
 
 import * as React from "react"
-import { useStaticQuery, graphql } from "gatsby"
+import { useStaticQuery, graphql, Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
+import styled from "@emotion/styled"
 
-const Bio = () => {
+function Bio() {
   const data = useStaticQuery(graphql`
     query BioQuery {
       site {
@@ -18,8 +19,9 @@ const Bio = () => {
             name
             summary
           }
+          description
           social {
-            twitter
+            github
           }
         }
       }
@@ -29,30 +31,85 @@ const Bio = () => {
   // Set these values by editing "siteMetadata" in gatsby-config.js
   const author = data.site.siteMetadata?.author
   const social = data.site.siteMetadata?.social
-
+  const description = data.site.siteMetadata?.description
   return (
-    <div className="bio">
+    <BioBox>
       <StaticImage
         className="bio-avatar"
         layout="fixed"
         formats={["auto", "webp", "avif"]}
-        src="../images/profile-pic.png"
-        width={50}
-        height={50}
+        src="../../images/profile-pic.png"
+        width={100}
+        height={100}
         quality={95}
         alt="Profile picture"
       />
       {author?.name && (
-        <p>
-          Written by <strong>{author.name}</strong> {author?.summary || null}
-          {` `}
-          <a href={`https://twitter.com/${social?.twitter || ``}`}>
-            You should follow them on Twitter
-          </a>
-        </p>
+        <BioTextBox>
+          <div className="name">
+            {author?.summary || null}{" "}
+            <Link className="aboutTag" to={"/about"}>
+              @{author.name}
+            </Link>
+          </div>
+          <div>{description}</div>
+          <Link className="link" to={`${social?.github || ``}`}>
+            <StaticImage
+              className="logo"
+              formats={["auto", "webp", "avif"]}
+              src={"../../images/github_logo.png"}
+              quality={95}
+              alt="Profile picture"
+            />
+            <span>Github</span>
+          </Link>
+        </BioTextBox>
       )}
-    </div>
+    </BioBox>
   )
 }
-
+const BioBox = styled.div`
+  display: flex;
+  height: fit-content;
+`
+const BioTextBox = styled.div`
+  padding: 5px 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  a {
+    text-decoration: none;
+  }
+  .name {
+    font-weight: 700;
+    font-size: 1.1em;
+  }
+  .aboutTag {
+    padding: 8px 10px;
+    border-radius: 10px;
+    color: #fff;
+    background-color: rgba(240, 62, 62, 0.8);
+    font-weight: 900;
+    &:hover {
+      border-color: var(--deepPink);
+      background-color: rgba(240, 62, 62, 0.6);
+    }
+  }
+  .link {
+    width: fit-content;
+    display: flex;
+    align-items: center;
+    gap: 4px;
+    color: var(--black);
+    padding: 4px 0;
+    border-radius: 10px;
+    .logo {
+      width: 20px;
+    }
+    &:hover {
+      background-color: #ced4da;
+      padding: 4px;
+    }
+  }
+`
 export default Bio
