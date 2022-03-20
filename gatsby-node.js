1c2ab006b3a6bda72484d73bb28ad0ebdb74c94d
0andme <yu00mi97@gmail.com>
2022-03-20 16:00:30 +0900

    conf :  GraphQL에 카테고리의 타입 스키마 적기

--- gatsby-node.js
+++ gatsby-node.js
@@ -106,6 +106,7 @@ exports.createSchemaCustomization = ({ actions }) => {
       title: String
       description: String
       date: Date @dateformat
+      categories:[String]
     }
 
     type Fields {
