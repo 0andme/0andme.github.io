c8dd4aa58e69210394aa7f0cf76ae78833b260ba
0andme <yu00mi97@gmail.com>
2022-03-20 11:39:48 +0900

    #7 delete :불필요한 코드 삭제

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -9,6 +9,5 @@ import "./src/normalize.css"
 // custom CSS styles
 import "./src/style.css"
 import "./src/color.css"
-import "./src/custom.css"
 // Highlighting for code blocks
 import "prismjs/themes/prism.css"
