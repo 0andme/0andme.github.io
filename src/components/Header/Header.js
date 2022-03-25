e58cfecc80c081aadaeb40427157ae5917518110
0andme <yu00mi97@gmail.com>
2022-03-25 15:52:13 +0900

    #1 feat : 헤더 기능 추가
    - about 이동 기능
    - github 이동 기능

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,8 @@
 import { css } from "@emotion/react"
+import PropTypes from "prop-types"
 import { graphql, useStaticQuery, Link } from "gatsby"
 import React from "react"
+import { StaticImage } from "gatsby-plugin-image"
 
 function Header({ isMain }) {
   const data = useStaticQuery(graphql`
@@ -8,6 +10,9 @@ function Header({ isMain }) {
       site {
         siteMetadata {
           subTitle
+          social {
+            github
+          }
         }
       }
     }
@@ -15,16 +20,38 @@ function Header({ isMain }) {
   return (
     <div css={HeaderStyle}>
       {!isMain && <Link to={"/"}>{data.site.siteMetadata.subTitle}</Link>}
+      <div className="right">
+        <Link to="/about">About</Link>
+        <a href={data.site.siteMetadata.social.github}>
+          <StaticImage
+            width={50}
+            height={50}
+            formats={["auto", "webp", "avif"]}
+            src={"../../images/github_logo.png"}
+            quality={95}
+            alt="0andme gitHub"
+          />
+        </a>
+      </div>
     </div>
   )
 }
 
+// props type
+Header.propTypes = {
+  isMain: PropTypes.bool,
+}
+// defatul props
+Header.defaultProps = {
+  isMain: false,
+}
 const HeaderStyle = css`
   width: 100%;
   height: 12vh;
   box-sizing: border-box;
   padding: 30px;
   display: flex;
+  justify-content: space-between;
   align-items: center;
   background: linear-gradient(to right, var(--primary), var(--deepPink));
   font-size: 2rem;
@@ -34,6 +61,11 @@ const HeaderStyle = css`
     color: #fff;
     opacity: 0.6;
     text-decoration: none;
+  }
+  .right {
+    display: flex;
+    align-items: center;
+    gap: 12px;
   }
 `
 export default Header
