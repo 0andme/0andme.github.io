cb9a551eb64d17a2ab637ca757212b33af891d79
0andme <yu00mi97@gmail.com>
2022-04-13 08:26:31 +0900

    #13 feat : 선택된 하위 카테고리가 있을 경우 고려

--- src/components/PostList/PostList.js
+++ src/components/PostList/PostList.js
@@ -2,7 +2,7 @@ import { css } from "@emotion/react"
 import React from "react"
 import PostItem from "./PostItem"
 
-function PostList({ selectCate, posts }) {
+function PostList({ subCate, selectCate, posts }) {
   // all일 때 모든 카테고리 리스트 출력
   if ("All" === selectCate) {
     return (
@@ -16,9 +16,21 @@ function PostList({ selectCate, posts }) {
   // 카테고리를 눌렀을 때
   // 해당 카테고리를 갖는 포스트 출력
   else {
-    const filterPosts = posts.filter(post => {
-      return post.frontmatter.categories.find(cate => cate === selectCate)
-    })
+    let filterPosts = []
+    // 선택된 하위 카테고리가 있다면
+    if (subCate) {
+      filterPosts = posts.filter(post => {
+        return (
+          post.frontmatter.categories?.find(cate => cate === selectCate) &&
+          post.frontmatter.subCategories?.find(cate => cate === subCate)
+        )
+      })
+    } else {
+      filterPosts = posts.filter(post => {
+        return post.frontmatter.categories?.find(cate => cate === selectCate)
+      })
+    }
+
     return (
       <ol css={PostListStyle}>
         {filterPosts.map(post => {
