d87eb7e3a42440f608d9dd0395311fd0177f76e3
0andme <yu00mi97@gmail.com>
2022-04-13 08:27:36 +0900

    #13 feat : 하위 카테고리 아이템 연결

--- src/components/CategoryListNav/CategoryList.js
+++ src/components/CategoryListNav/CategoryList.js
@@ -1,9 +1,16 @@
 import { css } from "@emotion/react"
 import { graphql, useStaticQuery } from "gatsby"
-import React from "react"
+import React, { useState } from "react"
 import CategoryItem from "./CategoryItem"
+import SubCategoryItem from "./SubCategoryItem"
 
-function CategoryList({ selectCate, setSelectCate, allPostNum }) {
+function CategoryList({
+  selectCate,
+  setSelectCate,
+  subCate,
+  setSubCate,
+  allPostNum,
+}) {
   const data = useStaticQuery(graphql`
     query {
       allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
@@ -15,30 +22,58 @@ function CategoryList({ selectCate, setSelectCate, allPostNum }) {
           }
           fieldValue
           totalCount
+          group(field: frontmatter___subCategories) {
+            edges {
+              node {
+                id
+              }
+            }
+            fieldValue
+            totalCount
+          }
         }
       }
     }
   `)
+
   const { group: cateList } = data.allMarkdownRemark
+  const subList = cateList.find(val => val.fieldValue === selectCate)?.group
   return (
-    <ul css={CategoryListStyle}>
-      <CategoryItem
-        selectCate={selectCate}
-        setSelectCate={setSelectCate}
-        item={{ fieldValue: "All", totalCount: allPostNum }}
-      />
-      {cateList.map(cateItem => {
-        const { fieldValue } = cateItem
-        return (
-          <CategoryItem
-            selectCate={selectCate}
-            setSelectCate={setSelectCate}
-            key={fieldValue}
-            item={cateItem}
-          />
-        )
-      })}
-    </ul>
+    <>
+      <ul css={CategoryListStyle}>
+        <CategoryItem
+          setSubCate={setSubCate}
+          selectCate={selectCate}
+          setSelectCate={setSelectCate}
+          item={{ fieldValue: "All", totalCount: allPostNum }}
+        />
+        {cateList.map(cateItem => {
+          const { fieldValue } = cateItem
+          return (
+            <CategoryItem
+              setSubCate={setSubCate}
+              selectCate={selectCate}
+              setSelectCate={setSelectCate}
+              key={fieldValue}
+              item={cateItem}
+            />
+          )
+        })}
+      </ul>
+      <ul css={CategoryListStyle}>
+        {subList?.map(cateItem => {
+          const { fieldValue } = cateItem
+          return (
+            <SubCategoryItem
+              subCate={subCate}
+              setSubCate={setSubCate}
+              key={fieldValue}
+              item={cateItem}
+            />
+          )
+        })}
+      </ul>
+    </>
   )
 }
 
@@ -50,5 +85,6 @@ const CategoryListStyle = css`
   flex-wrap: wrap;
   gap: 10px;
   list-style-type: none;
+  margin: 0;
 `
 export default CategoryList
