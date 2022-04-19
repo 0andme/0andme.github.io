b4f5dfbeb8defd8a2585839efe439b9849f49f6f
0andme <yu00mi97@gmail.com>
2022-04-19 11:39:12 +0900

    #14 feat : 목차 컴포넌트 기능 및 스타일 수정

--- src/components/TableOfContents/TableOfContents.js
+++ src/components/TableOfContents/TableOfContents.js
@@ -4,20 +4,30 @@ import React, { useState } from "react"
 function TableOfContents({ content }) {
   const [toggle, setToggle] = useState(true)
   return (
-    <TableStyle>
-      <ToggleBtn $toggle={toggle} onClick={() => setToggle(!toggle)}>
-        {toggle ? "목차" : "목차 보기"}
-      </ToggleBtn>
-      <div
-        className={"tocList " + (toggle ? "open" : "close")}
-        dangerouslySetInnerHTML={{ __html: content }}
-      ></div>
-    </TableStyle>
+    <>
+      {!toggle && (
+        <ToggleBtn $toggle={toggle} onClick={() => setToggle(!toggle)}>
+          {"목차 보기"}
+        </ToggleBtn>
+      )}
+      <TableStyle $toggle={toggle}>
+        <button onClick={() => setToggle(!toggle)}>
+          <img src={require("../../images/exit.svg").default} alt="나가기" />
+        </button>
+        <div
+          className={"tocList scroll "}
+          dangerouslySetInnerHTML={{ __html: content }}
+        ></div>
+      </TableStyle>
+    </>
   )
 }
 
 export default TableOfContents
 const ToggleBtn = styled.button`
+  position: fixed;
+  top: 38vh;
+  right: 0;
   background-color: var(--deepPink);
   color: #fff;
   font-weight: 700;
@@ -26,29 +36,77 @@ const ToggleBtn = styled.button`
     if ($toggle) return "100%"
   }};
   border: none;
-  border-radius: 12px;
+  border: 6px 0 6px 6px solid var(--primary);
+  border-radius: 12px 0 0 12px;
   padding: 10px;
 `
 const TableStyle = styled.div`
   position: fixed;
-  top: 12vh;
+  top: 10px;
   right: 0;
-  bottom: 0;
-  margin-top: var(--spacing-12);
-  padding: var(--spacing-10) var(--spacing-5);
+  bottom: 10px;
+  background-color: #fff;
+  border-radius: 12px 0 0 12px;
+  display: ${({ $toggle }) => {
+    if ($toggle) return "block"
+    else return "none"
+  }};
+  button {
+    position: absolute;
+    right: 0;
+    padding: 10px;
+    margin: 0;
+    border: none;
+    border-radius: 0 0 0 12px;
+    background-color: var(--deepPink);
+
+    img {
+      width: 30px;
+      height: 30px;
+    }
+  }
+  header {
+    position: absolute;
+    top: 0;
+    font-size: 2rem;
+    color: var(--midiGray);
+    background-color: yellow;
+  }
   .tocList {
+    margin: 16px;
+    z-index: 10;
+    height: calc(100vh - 32px - 20px);
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    overflow-y: scroll;
+
+    & > ul {
+      max-height: calc(100vh - 32px - 20px);
+      height: fit-content;
+      margin: 0;
+
+      & > li {
+        border-bottom: 1px dashed var(--deepPink);
+        &:last-child {
+          border-bottom: none;
+        }
+      }
+    }
+
     ul {
       list-style: none;
+      max-width: 30vw;
+
+      li {
+        margin: 0;
+        padding: 4px 0;
+      }
     }
+
     a {
       color: var(--black);
       text-decoration: none;
     }
-  }
-  .open {
-    display: block;
-  }
-  .close {
-    display: none;
   }
 `
